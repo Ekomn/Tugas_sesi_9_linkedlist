661c21e2addbbb513810c0fbf415161e9d576b0f
Ekomn <121342104+Ekomn@users.noreply.github.com>
2023-05-07 20:11:08 +0700

    Add files via upload

---
+++ Tugas_Sesi_9_1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Tugas_Sesi_9_1 { 
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)) {
+            // input data
+            System.out.println("");   
+            System.out.println("-------------------------------------------");   
+            System.out.println("DAFTAR AKTIFTAS SEHARI-HARI SENIN-JUMAT");  
+            System.out.println("Bangun tidur 4.30             : ok PROGRESS 100%");   
+            System.out.println("Mandi                         : ok PROGRESS 100%");   
+            System.out.println("Solat                         : ok PROGRESS 100%");   
+            System.out.println("Sarapan pagi                  : ok PROGRESS 100%");   
+            System.out.println("Berangkat kerja               : ok PROGRESS 100%");   
+            System.out.println("Makan siang                   : ok PROGRESS 100%");   
+            System.out.println("Pulang kerja jam 17.30        : ok PROGRESS 100%");
+            System.out.println("Mandi                         : ok PROGRESS 100%");  
+            System.out.println("Solat                         : ok PROGRESS 100%");
+            System.out.println("Mengerjakan tugas-tugas       : ok PROGRESS 100%");
+            System.out.println("Tidur 23.30                   : ok PROGRESS 100%");
+            
+           }
+    }
+}
